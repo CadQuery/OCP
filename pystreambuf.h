07f9aee1be0dddf70ee209814994a5bbd460f937
Adam Urbańczyk <adam-urbanczyk@users.noreply.github.com>
2021-05-04 23:14:49 +0200

    Use py::str instead of py::bytes when writing

--- pystreambuf.h
+++ pystreambuf.h
@@ -263,7 +263,7 @@ class streambuf : public std::basic_streambuf<char>
       }
       farthest_pptr = (std::max)(farthest_pptr, pptr());
       off_type n_written = (off_type)(farthest_pptr - pbase());
-      py::bytes chunk(pbase(), n_written);
+      py::str chunk(pbase(), n_written);
       py_write(chunk);
       if (!traits_type::eq_int_type(c, traits_type::eof())) {
         py_write(traits_type::to_char_type(c));
