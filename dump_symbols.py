7bef13a1525610a8c208537e1c6793cf71d27334
AU <adam-urbanczyk@users.noreply.github.com>
2023-11-03 06:21:36 +0100

    OCCT 7.7.2 (#125)
    * OCCT 7.7.2
    
    * Update env definition
    
    * Use newer boost
    
    * Bump occt version in the conda recipe
    
    * Explicit Python version
    
    * Specify root dir
    
    * More find python tweaks
    
    * Specify params for findpython3 and findpython
    
    * Call dump symbols from cmake
    
    * Remove symbol files
    
    * Added py-lief
    
    * Use newer conda-build
    
    * Skip GetPalette
    
    * More tweaks
    
    * Set the python version correctly
    
    * Typo fix
    
    * Simpler env creation
    
    * Less strict build env
    
    * Do not spec libcxx
    
    * back to libcxx
    
    * Try clangdev 15
    
    * Less include dirs on mac
    
    * Use newer SDK
    
    * Use c++ headers form the mac sdk
    
    * Cleanup

--- dump_symbols.py
+++ dump_symbols.py
@@ -1,46 +1,33 @@
 import lief
 import sys
-from path import Path
 
 from logzero import logger
 
-prefix = sys.argv[-1]
-
-prefix_mac = (Path(prefix) / 'lib_mac').expand()
-libs_mac = prefix_mac.glob('**/libTK*.*.*.dylib')
-
-prefix_linux = (Path(prefix) / 'lib_linux').expand()
-libs_linux = prefix_linux.glob('**/libTK*.so.*.*.*')
-
-prefix_win = (Path(prefix) / 'lib_win').expand()
-libs_win = prefix_win.glob('**/**/TK*.dll')
+libs = sys.argv[-1].split(';')
+name = {'win32' : 'win', 'darwin' : 'mac', 'linux' : 'linux'}[sys.platform]
 
 
-logger.info(libs_win)
+exported_symbols = []
 
-for name,libs in {'linux' : libs_linux,'mac' : libs_mac, 'win' : libs_win}.items():
+for lib in libs:
 
-    logger.info(libs)
+    logger.info(f'Analyzing {lib}')
 
-    exported_symbols = []
-    
-    for lib in libs:
+    p = lief.parse(lib)
 
-        p = lief.parse(lib)
+    if p is None:
+        continue
 
-        if p is None:
-            continue
-        
-        if name=='linux':
-            for s in p.exported_symbols:
+    if name=='linux':
+        for s in p.exported_symbols:
+            exported_symbols.append(f'{s.name}\n')
+    elif name=='mac':
+        for s in p.symbols:
+            if s.type>1:
                 exported_symbols.append(f'{s.name}\n')
-        elif name=='mac':
-            for s in p.symbols:
-                if s.type>1:
-                    exported_symbols.append(f'{s.name}\n')
-        else:
-            for s in p.exported_functions:
-                exported_symbols.append(f'{s.name}\n')
-            
-    with open(f'symbols_mangled_{name}.dat','w') as f:
-        f.writelines(exported_symbols)
+    else:
+        for s in p.exported_functions:
+            exported_symbols.append(f'{s.name}\n')
+
+with open(f'symbols_mangled_{name}.dat','w') as f:
+    f.writelines(exported_symbols)
