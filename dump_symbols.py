3c73ff4fc5c4910a56f8b08de5d02bf92f850064
AU <adam-urbanczyk@users.noreply.github.com>
2025-07-27 21:54:10 +0200

    7.9 fixes based on testing with CQ (#177)
    * Adding 7.9
    
    * Update config
    
    * Update env
    
    * Start reworking cmakelists
    
    * Update CI
    
    * Back to 2 jobs by default
    
    * Use newer ubuntu
    
    * conda run iso mamba run
    
    * Roll back to 1.5
    
    * Use python 3.12 in the base env
    
    * Update 2nd conda-enable
    
    * Add some exclusions
    
    * Update pywrap
    
    * Update pywrap
    
    * Update pywrap
    
    * Add extra includes to fix compilation errors
    
    * Update pywrap
    
    * Add some missing headers
    
    * Extra exclude
    
    * Update pywrap
    
    * Use newer image
    
    * Use newer python
    
    * Use clang 20
    
    * Trying to resolve the env
    
    * Even newer image
    
    * Update msvc location
    
    * Use one core on win
    
    * Update meta.yaml
    
    * Update pywrap
    
    * Build out of tree
    
    * Generete in the current dir for now
    
    * Update occt version in meta.yaml
    
    * Update pywrap
    
    * Try installing g++-13
    
    * Try nostdinc++ on linux
    
    * Remove  mesa-libgl-devel-cos6-x86_64
    
    * Revert additional flags on linux
    
    * Use cxx-compiler also on win
    
    * Pipeline cleanup
    
    * Use stock stubgen
    
    * Use stock stubgen
    
    * Fix the pybind11_stubgen call
    
    * Rename local stub dir
    
    * Back to 2019
    
    * Newer python
    
    * Try with win-latest
    
    * Update pywrap
    
    * Add other smart ptr names
    
    * Update pywrap
    
    * Exclude ns via configuration
    
    * More ns exlucdes
    
    * Typo fix
    
    * Switch to micromamba
    
    * Use micromamba everywhere
    
    * Replace more conda occurances
    
    * Use correct options
    
    * Fix additional mamba invocations
    
    * Swtich to compilation in bash on windows
    
    * Explicit micromamba init for win compilation
    
    * Preparing for x-generation
    
    * Try x-generating for win
    
    * Fix name
    
    * Typo fix
    
    * Fix dir
    
    * Try to fix platform handling
    
    * Use own windows.h
    
    * Update pywrap
    
    * Be more verbose
    
    * Always use extern/include
    
    * Compile in an env explicitely
    
    * Back to pwsh
    
    * Use cmake -E on win
    
    ---------
    
    Co-authored-by: adam-urbanczyk <13981538+adam-urbanczyk@users.noreply.github.com>
    Co-authored-by: adam-urbanczyk <adamj@79LABS>

--- dump_symbols.py
+++ dump_symbols.py
@@ -4,8 +4,6 @@ import sys
 from logzero import logger
 
 libs = sys.argv[-1].split(';')
-name = {'win32' : 'win', 'darwin' : 'mac', 'linux' : 'linux'}[sys.platform]
-
 
 exported_symbols = []
 
@@ -14,18 +12,22 @@ for lib in libs:
     logger.info(f'Analyzing {lib}')
 
     p = lief.parse(lib)
+    format = p.format
 
     if p is None:
         continue
 
-    if name=='linux':
+    if format==p.FORMATS.ELF:
+        name = "linux"
         for s in p.exported_symbols:
             exported_symbols.append(f'{s.name}\n')
-    elif name=='mac':
+    elif format==p.FORMATS.MACHO:
+        name = "mac"
         for s in p.symbols:
             if s.raw_type>1:
                 exported_symbols.append(f'{s.name}\n')
     else:
+        name = "win"
         for s in p.exported_functions:
             exported_symbols.append(f'{s.name}\n')
 
